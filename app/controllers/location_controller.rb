4437d83aad0ff2e3ed68773ec5c866ff681c30ec
dgleiche <dgleiche@me.com>
2018-12-02 00:56:21 -0500

    Added API

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,5 +1,63 @@
 class LocationController < ApplicationController
 
-  
+  def location_info
+    google_place_id = params[:google_place_id]
+    location = Location.find_by(google_place_id: google_place_id).first
+    accessibility_informations = AccessibilityInformation.find_by(location: location)
+
+    render json: [location: location.to_json, information: accessibility_informations.to_json].to_json
+  end
+
+  def add_location
+    location = Location.create(location_params)
+
+    render json: location.to_json
+  end
+
+  def update_location
+    location_id = params[:location][:id]
+    location = Location.find(location_id)
+
+    location.update(location_params)
+
+    render json: location.to_json
+  end
+
+  def add_accessibility_information
+    information = AccessibilityInformation.create(accessibility_information_params)
+
+    render json: information.to_json
+  end
+
+  def update_accessibility_information
+    information_id = params[:accessibility_information][:id]
+    information = AccessibilityInformation.find(information_id)
+
+    information.update(accessibility_information_params)
+
+    render json: information.to_json
+  end
+
+  def location_params
+    location_type_name = params[:location][:location_type_name]
+    unless location_type_name.nil?
+      location_type = LocationType.find_or_create_by(name: location_type_name).first
+      params[:location][:location_type_id] = location_type.id
+    end
+
+    params.require(:location).permit(:name, :address, :city, :state, :zip, :lat, :long, :google_place_id, :location_type_id)
+  end
+
+  def accessibility_information_params
+    google_place_id = params[:accessibility_information][:google_place_id]
+    unless google_place_id.nil?
+      location_id = Location.find_by(google_place_id: google_place_id).first
+      params[:accessibility_information][:location_id] = location_id
+    end
+
+    params.require(:accessibility_information).permit(:description, :gen_accessible, :smooth, :elevators, :ramps, :sound,
+                                                      :sound_measure, :brail, :sight_impaired, :bathrooms, :automatic_doors,
+                                                      :parking, :photos, :door_widths, :table_heights, :location_id, :score)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,102 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # INPUT FORMAT:
+  # {
+  #   google_place_id: Integer (required)
+  # }
+  # RETURNS:
+  # {
+  #   location: JSON object representing location data
+  #   information: JSON object representing accessibility information data
+  # }
+  post 'location/get' => 'location#location_info'
 
+
+  # INPUT FORMAT:
+  # {location:
+  #   location_type_name: String
+  #   name: String
+  #   address: String
+  #   city: String
+  #   state: String
+  #   zip: String
+  #   lat: Float
+  #   long: Float
+  #   google_place_id: Integer
+  #   location_type_id: Integer (replaced if location_type_name is specified)
+  # }
+  # RETURNS:
+  # JSON object representing inputted location
+  post 'location/add_location' => 'location#add_location'
+
+  # INPUT FORMAT:
+  # {location:
+  #   id: Integer (REQUIRED)
+  #   location_type_name: String
+  #   name: String
+  #   address: String
+  #   city: String
+  #   state: String
+  #   zip: String
+  #   lat: Float
+  #   long: Float
+  #   google_place_id: Integer
+  #   location_type_id: Integer (replaced if location_type_name is specified)
+  # }
+  # RETURNS:
+  # JSON object representing updated location
+  post 'location/update_location' => 'location#update_location'
+
+  # INPUT FORMAT:
+  # {accessibility_information:
+  #   google_place_id: Integer
+  #   location_id: Integer (replaced if google_place_id is specified)
+  #   description: String
+  #   gen_accessible: Bool
+  #   smooth: Bool
+  #   elevators: Bool
+  #   ramps: Bool
+  #   sound: Bool
+  #   sound_measure: Float
+  #   brail: Bool
+  #   sight_impaired: Bool
+  #   bathrooms: Bool
+  #   automatic_doors: Bool
+  #   parking: Bool
+  #   photos: String[] (array of URLs)
+  #   door_widths: Float[] (array of door widths)
+  #   table_heights: Float[] (array of table heights)
+  #   score: Float (average of the bools)
+  # }
+  # RETURNS:
+  # JSON object representing inputted accessibility information
+  post 'location/add_information' => 'location#add_accessesibility_information'
+
+  # INPUT FORMAT:
+  # {accessibility_information:
+  #   id: Integer (REQUIRED)
+  #   google_place_id: Integer
+  #   location_id: Integer (replaced if google_place_id is specified)
+  #   description: String
+  #   gen_accessible: Bool
+  #   smooth: Bool
+  #   elevators: Bool
+  #   ramps: Bool
+  #   sound: Bool
+  #   sound_measure: Float
+  #   brail: Bool
+  #   sight_impaired: Bool
+  #   bathrooms: Bool
+  #   automatic_doors: Bool
+  #   parking: Bool
+  #   photos: String[] (array of URLs)
+  #   door_widths: Float[] (array of door widths)
+  #   table_heights: Float[] (array of table heights)
+  #   score: Float (average of the bools)
+  # }
+  # RETURNS:
+  # JSON object representing updated accessibility information
+  post 'location/update_information' => 'location#update_accessibility_information'
 
 end
